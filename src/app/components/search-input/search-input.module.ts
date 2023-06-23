c438e3cbfa6baa48791f35d7610cd260922d783f
Lucas Lessa <lucaslessa14@hotmail.com>
2023-06-23 12:13:56 -0300

    feat: create started components

---
+++ src/app/components/search-input/search-input.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchInputComponent } from './search-input.component';
+
+@NgModule({
+  declarations: [
+    SearchInputComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SearchInputComponent
+  ]
+})
+export class SearchInputModule { }
