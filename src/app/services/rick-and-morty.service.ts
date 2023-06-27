39c1260c325215d2c4c436a123232ac6114a74fb
Lucas Lessa <lucaslessa14@hotmail.com>
2023-06-27 12:04:06 -0300

    feat: getCaracters can recieve multiples options to make a request

--- src/app/services/rick-and-morty.service.ts
+++ src/app/services/rick-and-morty.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CaractersAPIResponse } from '../interfaces/caracters.interfaces';
 
@@ -12,7 +12,25 @@ export class RickAndMortyService {
 
   constructor(private http: HttpClient) { }
 
-  public getCharacters(): Observable<CaractersAPIResponse> {
-    return this.http.get<CaractersAPIResponse>(this.URL);
+  public getCharacters(names?: string[], statuses?: string[], species?: string[], types?: string[], genders?: string[]): Observable<CaractersAPIResponse> {
+    let params = new HttpParams();
+
+    if (names && names.length > 0) {
+      params = params.set('name', names.join(','));
+    }
+    if (statuses && statuses.length > 0) {
+      params = params.set('status', statuses.join(','));
+    }
+    if (species && species.length > 0) {
+      params = params.set('species', species.join(','));
+    }
+    if (types && types.length > 0) {
+      params = params.set('type', types.join(','));
+    }
+    if (genders && genders.length > 0) {
+      params = params.set('gender', genders.join(','));
+    }
+
+    return this.http.get<CaractersAPIResponse>(this.URL, { params: params });
   }
 }
