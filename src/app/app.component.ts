078fe7aee4275f85e3d34536ca56af28da11726d
lessa-portfolio <eng.lucaslessa@gmail.com>
2024-01-04 15:15:25 -0300

    WIP: update app to use standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, HostListener, ElementRef } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { RickAndMortyService } from './services/rick-and-morty.service';
 import { Info, Result } from './interfaces/caracters.interfaces';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
+// @ts-ignore
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
